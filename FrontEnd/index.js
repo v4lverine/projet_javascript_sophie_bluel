dfb8b9ae5b0312dac1b8ebd78a3c40422553bba7
Valentine Bontemps <valveryne@hotmail.com>
2023-01-10 16:43:42 +0100

    Récupération données API

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -3,14 +3,35 @@ console.log("Hello world !");
 fetch("http://localhost:5678/api/categories")
 .then(function(res){
     if (res.ok) {
-        return res.json
+        return res.json();
     }
 })
 
 .then(function(value){
-    console.log()
+    console.log(value);
 })
 
 .catch(function(err){
-    console.log("J'ai eu une erreur !")
+    console.log("J'ai eu une erreur !");
+});
+
+
+fetch("http://localhost:5678/api/works")
+.then(function(res){
+    if (res.ok) {
+        return res.json();
+    }
+})
+
+.then(function(value){
+    console.log(value);
+    let photosDuSite = document.getElementById("photographs");
+    photosDuSite.innerHTML = "";
+    for (let photos of value) {//va parcourir value et va le mettre dans photos et exécutera les lignes mis dans la boucle
+        photosDuSite.innerHTML += "<figure><img src=\"" + photos["imageUrl"] + "\" alt=\"" + photos["title"] + "\" ><figcaption>" + photos["title"] + "</figcaption></figure>";
+    }
+})
+
+.catch(function(err){
+    console.log("J'ai eu une erreur !");
 });

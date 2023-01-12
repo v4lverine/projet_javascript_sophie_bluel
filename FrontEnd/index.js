78ac838e20390b7490ee1c0d5c47284ab1b9df14
Valentine Bontemps <valveryne@hotmail.com>
2023-01-12 17:01:29 +0100

    Ajout de nombreuses choses dans FrontEnd

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,8 +1,8 @@
 console.log("Hello world !");
 
 
-function addFigures(photos){ //Définition de la fonction addFigures, une fonction permet de condenser du code
-    const newFigure = document.createElement("figure");
+function addFigures(photos){ //Définition et déclaration de la fonction addFigures, une fonction permet de condenser du code
+    const newFigure = document.createElement("figure"); //affectera le DOM
 
     const imageFigure = document.createElement("img");
     imageFigure.src = photos["imageUrl"];
@@ -13,27 +13,45 @@ function addFigures(photos){ //Définition de la fonction addFigures, une foncti
     const captionFigure = document.createElement("figcaption"); //création de HTML qui va impacter sur le CSS existant
     captionFigure.innerText = photos["title"];
 
-    newFigure.appendChild(imageFigure);//appendChild met à la suite l'élément
+    newFigure.appendChild(imageFigure);//appendChild ajoute l'élément à newFigure, met à la suite l'élément
     newFigure.appendChild(captionFigure);
 
     return newFigure; //la fonction donne une valeur en réponse, sans ça, la fonction ne renvoit rien
 }
 
 
-fetch("http://localhost:5678/api/works") //fetch = appel à une fonction, ce fetch appelle aux travaux dans l'API
+fetch("http://localhost:5678/api/works") //fetch = appel à une fonction, ce fetch appelle aux travaux dans l'API, utilisation d'une fonction
 .then(function(res){
     if (res.ok) {
-        return res.json();
+        return res.json(); //récupère le json ici
     }
 })
 
-.then(function(listPictures){
+.then(function(listPictures){ //va traiter les données
     console.log(listPictures);
     let websitePictures = document.getElementById("photographs");
     websitePictures.innerHTML = ""; //vide le div avec les figures
-    for (let photos of listPictures) {//va parcourir value et va le mettre dans photos et exécutera les lignes mis dans la boucle
+
+    let websiteCategories = document.getElementById("categories");
+
+    const setCategories = new Set(); //set pour les catégories (tags)
+
+    for (let photos of listPictures) {//va parcourir listPictures et va le mettre dans photos et exécutera les lignes mises dans la boucle
 
         websitePictures.appendChild(addFigures(photos)); //appel à la fonction qui comprend tout ce qui est dans la définition de la fonction, va exécuter ce qu'il y a dedans
+        
+        setCategories.add(photos["category"]["name"]); //permet de faire un tri des doublons (je crois), on retrouvera que les catégories uniques       
+    }
+
+    for (let category of setCategories){  //dans une boucle, on recrée toujours une variable car elle n'existe que dans celle-ci
+        const newCategory = document.createElement("li");
+        
+        const newTag = document.createElement("a");
+        newTag.innerText = category;
+
+        newCategory.appendChild(newTag); //il faut maintenant ajouter le filtrage des données des id provenant de l'API
+
+        websiteCategories.appendChild(newCategory);
     }
 })
 
@@ -44,18 +62,36 @@ fetch("http://localhost:5678/api/works") //fetch = appel à une fonction, ce fet
 
 
 
-fetch("http://localhost:5678/api/categories") //ce fetch appelle aux catégories dans l'API
-.then(function(res){
-    if (res.ok) {
-        return res.json();
-    }
-})
 
-.then(function(tags){
-    console.log(tags);
-})
 
-.catch(function(err){
-    console.log("J'ai eu une erreur !");
-});
+// fetch("http://localhost:5678/api/categories") //ce fetch appelle aux catégories dans l'API
+// .then(function(res){
+//     if (res.ok) {
+//         return res.json();
+//     }
+// })
+
+// .then(function(tags){ // récupère le json ici
+//     console.log(tags);
+
+
+
+//     for (let tag of tags){ //va passer sur chaque élément de tags et va les mettre dans tag et va exécuter les lignes entre les accolades pour chacun
+
+//     }
+
+//     for (let category of setCategories){  //dans une boucle, on recrée toujours une variable car elle n'existe que dans celle-ci
+//         const newCategory = document.createElement("li");
+        
+//         const newTag = document.createElement("a");
+//         newTag.id = category["id"];
+//         newTag.innerText = category["name"];
+
+//         newCategory.appendChild(newTag);
+//     }
+// })
+
+// .catch(function(err){
+//     console.log("J'ai eu une erreur !");
+// });
 
